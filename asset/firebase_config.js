83b9e0ef849752c8dec886c348faf688a50521c1
khakitoad <117ysc@gmail.com>
2026-07-28 00:11:25 +0800

    Create firebase_config.js

---
+++ asset/firebase_config.js
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc, getDocs, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB-sXElOvlT2flS8GVLWBWYQ8JdpTUJAWs",
+  authDomain: "chive-club-website.firebaseapp.com",
+  projectId: "chive-club-website",
+  storageBucket: "chive-club-website.firebasestorage.app",
+  messagingSenderId: "33996148800",
+  appId: "1:33996148800:web:30e51818a0023ed61acfa5",
+  measurementId: "G-WD88DLKEVC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+async function addSignupData(name, email, city, message) {
+    try {
+        const docRef = await addDoc(collection(db, "signup"), {
+            name: "Hello World",
+            email: email,
+            city: city,
+            message: message,
+            time: serverTimestamp()
+        });
+        console.log("Saved successfully! ID:", docRef.id);
+    } catch (error) {
+        console.error("Error saving data:", error);
+    }
+}
+
+export { addSignupData };
