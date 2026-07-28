9c744ba30265253b6587332b277814a11112fe3a
khakitoad <117ysc@gmail.com>
2026-07-28 22:56:49 +0800

    Update batch-send.js

--- asset/batch-send.js
+++ asset/batch-send.js
@@ -1,7 +1,11 @@
 import { sendSignupEmail } from './sendEmail.js';
 import { getAllSignupData } from './firebase.js';
 
-const allData = getAllSignupData;
-allData.forEach(item => {
-  sendSignupEmail(item.email);
-});
+function sendDailyEmail() {
+  const allData = getAllSignupData;
+  allData.forEach(item => {
+    sendSignupEmail(item.email);
+  });
+}
+
+export { sendDailyEmail };
