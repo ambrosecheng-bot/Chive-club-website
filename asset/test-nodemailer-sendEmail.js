2e6796ea850faaca689c3ad5ad5ecd1e66b0d3bf
sunnymoksoton <ylm1e25@soton.ac.uk>
2026-07-30 22:14:01 +0800

    Update test-nodemailer-sendEmail.js

--- asset/test-nodemailer-sendEmail.js
+++ asset/test-nodemailer-sendEmail.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-function sendSignupEmail(userEmail) {
+async function sendSignupEmail(userEmail) {
   try {
     const info = await transporter.sendMail({
       from: '"Sunny Mok" <mehavenodad@gmail.com>', // sender address
