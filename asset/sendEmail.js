05434ba078d46a4c71bf1c86f0aab02c7a3fa755
sunnymoksoton <ylm1e25@soton.ac.uk>
2026-07-31 14:36:27 +0800

    Update sendEmail.js

--- asset/sendEmail.js
+++ asset/sendEmail.js
@@ -3,8 +3,8 @@ emailjs.init(/*"XKOeiuX5zHgQT4aqf"*/ "BXtcg16hSID5oBKDc"); //switched to sunnyya
 
 function sendSignupEmail(userEmail) {
     // These IDs come from your EmailJS dashboard after setup
-    const serviceID = /*"service_55s2sal"*/ "service_1wh8ska"; //switched to sunnyyanlongmok's emailjs (temporary)
-    const templateID = /*"template_v48jvlt"*/ "template_c34qvk2";
+    const serviceID = "service_55s2sal" // "service_1wh8ska"; //commented is sunnyyanlongmok's emailjs 
+    const templateID = "template_v48jvlt" // "template_c34qvk2";
 
     // Match these keys exactly with the {{variable_names}} inside your EmailJS template
     const templateParams = {
